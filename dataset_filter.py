2efe7010321842e26e539449c9a2685a1a644ae8
Ekaterina Ledeneva <cthfvrl@gmail.com>
2020-03-27 21:38:14 +0300

    Random bin filtering

--- dataset_filter.py
+++ dataset_filter.py
@@ -4,11 +4,12 @@ from visualization import dataset_visualizer as visualizer
 from similarities.lcs_similarity import LCSSimilarity
 import numpy as np
 from scipy.stats import chisquare
+from random import sample
 
 
 def binned(data, num):
     n = len(data)
-    n_bins = max(n // num, 1)
+    n_bins = visualizer.get_bins(n) if num is None else max(n // num, 1)
     delta = 1 / n_bins
 
     similarity = LCSSimilarity()
@@ -40,7 +41,7 @@ def filter_negative_data(filename, num):
 
     bins = binned(data_neg, num)
     bound = int(upper(list(map(len, bins))))
-    bins = list(map(lambda b: b[:bound], bins))
+    bins = list(map(lambda b: sample(b, min(bound, len(b))), bins))
     return data_pos + [item for sublist in bins for item in sublist]
 
 
@@ -48,7 +49,7 @@ def filter_all_data(filename, num):
     data = parser.list_from_file(filename)
     bins = binned(data, num)
     bound = int(upper(list(map(len, bins))))
-    bins = list(map(lambda b: b[:bound], bins))
+    bins = list(map(lambda b: sample(b, min(bound, len(b))), bins))
     return [item for sublist in bins for item in sublist]
 
 
@@ -57,8 +58,8 @@ def chi_test(data, num):
     return chisquare(list(map(len, bins)))
 
 
-def run_filter(file_in, file_out, num):
-    filtered = filter_negative_data(file_in, num)
+def run_filter(file_in, file_out, num=None):
+    filtered = filter_all_data(file_in, num)
     parser.list_to_file(filtered, file_out)
 
     ## Гипотеза принимается, если pvalue > alpha
@@ -70,12 +71,23 @@ def run_filter(file_in, file_out, num):
     visualizer.visualize_from_file(file_out)
 
 
+def print_statistics(filename):
+    data = parser.list_from_file(filename)
+    print('Filename:', filename)
+    print('Dataset size:', len(data))
+    data_neg = list(filter(lambda x: x[2] == '0', data))
+    data_pos = list(filter(lambda x: x[2] == '1', data))
+    print('Negative class:', len(data_neg))
+    print('Positive class:', len(data_pos))
+
+
 if __name__ == '__main__':
     #pass
-    # run_filter('data.csv', 'filtered.csv', 100)
-    # run_filter('filtered.csv', 'filtered2.csv', 100)
-    # visualizer.visualize_from_file('filtered.csv')
-    # run_filter('data_junit4_new_full.csv', 'data_junit4_new_filtered.csv', 700)
-    # run_filter('data_gson_full.csv', 'data_gson_filtered.csv', 20)
-     run_filter('data_mockito_full.csv', 'data_mockito_filtered.csv', 400)
-    # visualizer.visualize_from_file('data_mockito_full.csv')
+    # run_filter('datasets/junit4_filtered.csv', 'datasets/junit4_filtered2.csv')
+    # visualizer.visualize_from_file('datasets/filtered.csv')
+
+    # print_statistics('datasets/gson.csv')
+    # print_statistics('datasets/junit4.csv')
+    # print_statistics('datasets/mockito.csv')
+    # print_statistics('datasets/slf4j.csv')
+    print_statistics('datasets/filtered.csv')
