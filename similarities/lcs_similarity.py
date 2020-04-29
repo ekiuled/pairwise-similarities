c3ba4ef2ab281a73be8f2d0fbbcd5e59eafece7a
Ekaterina Ledeneva <cthfvrl@gmail.com>
2020-04-29 18:53:58 +0300

    LCS on a word boundary

--- similarities/lcs_similarity.py
+++ similarities/lcs_similarity.py
@@ -1,9 +1,20 @@
 from similarities.similarity import Similarity
 import difflib
+from string import punctuation
+
+table = str.maketrans('', '', punctuation)
 
 
 class LCSSimilarity(Similarity):
     def similarity(self, x, y):
-        sm = difflib.SequenceMatcher()
-        sm.set_seqs(x, y)
-        return sm.ratio()
+        x = x.translate(table).split()
+        y = y.translate(table).split()
+        sm = difflib.SequenceMatcher(None, x, y)
+        blocks = sm.get_matching_blocks()[:-1]
+        matching = 0
+        x = list(map(len, x))
+        y = list(map(len, y))
+        for start, _, length in blocks:
+            matching += sum(x[start:start+length])
+        total = sum(x) + sum(y)
+        return 2 * matching / total
